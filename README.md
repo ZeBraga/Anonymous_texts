5341b2a784d3e41b6a5a2042a00eda8a30214ecf
José Augusto Azevedo Genovez de Mesquita Braga <joseaugusto.braga@gmail.com>
2024-09-12 11:35:26 -0300

    Add scrips `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Anonymous_texts
- This repository was created to implement a site where anyone can post poems or texts with a pseudonym.
+
+This repository was created to implement a site where anyone can post poems or texts with a pseudonym.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>This is the beginning of a big project to create an environment to anyone write and post their texts!</h1>
+function Home() {
+  return (
+    <h1>
+      This is the beginning of a big project to create an environment to anyone
+      write and post their texts!
+    </h1>
+  );
 }
 
 export default Home;
